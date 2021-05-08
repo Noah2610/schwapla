fd44aad84c953007194e990e8391dd9fb2c989fb
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-05-08 08:09:44 +0200

    Add ability to scroll on sliders to change values
    Unfinished, still has some problems.

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef } from "react";
 import classNames from "classnames";
 import styles from "./Slider.module.scss";
 
@@ -6,7 +7,7 @@ export interface SliderProps {
     min: number;
     max: number;
     step: number;
-    onChange?: (value: number) => void;
+    onChange: (value: number) => void;
 }
 
 export default function Slider({
@@ -16,6 +17,47 @@ export default function Slider({
     step,
     onChange,
 }: SliderProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const dataRef = useRef({ value, min, max, step });
+    dataRef.current = { value, min, max, step };
+
+    const incr = useCallback(() => {
+        const { value, step, max } = dataRef.current;
+        const newValue = Math.min(value + step, max);
+        if (newValue !== value) {
+            onChange(newValue);
+        }
+    }, []);
+
+    const decr = useCallback(() => {
+        const { value, step, min } = dataRef.current;
+        const newValue = Math.max(value - step, min);
+        if (newValue !== value) {
+            onChange(newValue);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (inputRef.current) {
+            const onWheel = (event: WheelEvent) => {
+                event.preventDefault();
+                switch (Math.sign(event.deltaY)) {
+                    case 1:
+                        decr();
+                        break;
+                    case -1:
+                        incr();
+                        break;
+                }
+            };
+
+            inputRef.current.addEventListener("wheel", onWheel);
+
+            return () =>
+                inputRef.current?.removeEventListener("wheel", onWheel);
+        }
+    }, [inputRef, incr, decr]);
+
     return (
         <input
             className={classNames("input", styles.slider)}
@@ -24,15 +66,13 @@ export default function Slider({
             min={min}
             max={max}
             step={step}
-            onChange={
-                onChange &&
-                ((e) => {
-                    const newValue = parseFloat(e.currentTarget.value);
-                    if (newValue || newValue === 0) {
-                        onChange(newValue);
-                    }
-                })
-            }
+            onChange={(e) => {
+                const newValue = parseFloat(e.currentTarget.value);
+                if (newValue || newValue === 0) {
+                    onChange(newValue);
+                }
+            }}
+            ref={inputRef}
         />
     );
 }
